670f6df6fa9018b6fe476d1de82286eb32b41e86
Bob Copeland <me@bobcopeland.com>
2017-04-17 07:49:51 -0400

    implement save backend

--- src/crossy/server.go
+++ src/crossy/server.go
@@ -5,12 +5,14 @@ package main
 //  fetching /puzzle/id gets puz-format fill - DONE
 //  server parses puz format into something more reasonable
 //  fetching /solve/id gets current solution
+//    fetch anything works DONE
+//    solution saved in backend store
 //  anyone can post /solve/
 //  database as backend store
 //
 //  bobcopeland.com/crosswords/xyzzy
-//   --> embeds xwordjs with id=xyzzy
-//   --> xwordjs periodically fetches /solve/xyzzy
+//   --> embeds xwordjs with id=xyzzy (send in uri for now) DONE
+//   --> xwordjs periodically fetches /solve/xyzzy DONE
 //   --> xwordjs periodically posts /solve/xyzzy for updates
 //   --> if first solver, start the clock
 //    --> respond with current fill
@@ -24,6 +26,7 @@ import (
 	"mime/multipart"
 	"net/http"
 	"os"
+	"strings"
 )
 
 type User struct {
@@ -44,11 +47,16 @@ type PuzzleUploadResponse struct {
 type SolveGetResponse struct {
 	Id      string
 	Version int
-	Fill    string
+	Fill    []string
 }
 
 func (c *Context) Auth(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
 	c.user.name = "bobc"
+	next(rw, req)
+}
+
+func (c *Context) Headers(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
+	rw.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	next(rw, req)
 }
 
@@ -84,11 +92,29 @@ func SaveFile(file multipart.File) (id string) {
 }
 
 func LoadFile(id string) (file *os.File) {
+	// TODO check id for sanity
 	f, err := os.OpenFile("./store/"+id+".puz", os.O_RDONLY, 0)
 	if err != nil {
 		panic("could not load file")
 	}
 	return f
+}
+
+func (c *Context) SolveGet(rw web.ResponseWriter, req *web.Request) {
+	id := req.PathParams["id"]
+	r := SolveGetResponse{}
+	r.Id = id
+	r.Version = 2
+	r.Fill = make([]string, 21)
+	for i := range r.Fill {
+		r.Fill[i] = strings.Repeat(" ", 21)
+	}
+	r.Fill[0] = "abcde#xxxx#yyyy#zzzzz"
+	b, err := json.Marshal(r)
+	if err != nil {
+		panic("cannot generate json response")
+	}
+	fmt.Fprint(rw, string(b))
 }
 
 func (c *Context) PuzzleUpload(rw web.ResponseWriter, req *web.Request) {
@@ -129,8 +155,10 @@ func main() {
 		Middleware(web.LoggerMiddleware).
 		Middleware(web.ShowErrorsMiddleware).
 		Middleware((*Context).Auth).
+		Middleware((*Context).Headers).
 		Get("/puzzle/:id", (*Context).PuzzleGet).
 		Get("/puzzle/upload", (*Context).PuzzleUploadGet).
+		Get("/solve/:id", (*Context).SolveGet).
 		Post("/puzzle/", (*Context).PuzzleUpload)
 	http.ListenAndServe("localhost:4000", router)
 }
